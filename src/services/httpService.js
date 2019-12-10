8cd9eafd4a7d18124e0a3ce4e444981c2bc89bfd
Tim Williams <jtimwill@gmail.com>
2019-12-09 22:10:35 -0500

    Update httpService

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// axios.defaults.baseURL = process.env.REACT_APP_API_URL;
-axios.defaults.baseURL = 'https://nameless-peak-09747.herokuapp.com/api';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 axios.interceptors.response.use(null, error => {
   const expectedError =
